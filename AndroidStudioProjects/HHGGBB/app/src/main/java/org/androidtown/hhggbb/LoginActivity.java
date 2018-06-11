cf5bf616b137fcf6c6d5a43980c6cdded48596e4
annab96 <lannab082@gmail.com>
2018-06-11 20:24:59 +0900

    6.11

--- AndroidStudioProjects/HHGGBB/app/src/main/java/org/androidtown/hhggbb/LoginActivity.java
+++ AndroidStudioProjects/HHGGBB/app/src/main/java/org/androidtown/hhggbb/LoginActivity.java
@@ -1,13 +1,21 @@
 package org.androidtown.hhggbb;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
-}
+        public void onButton1Clicked(View v){
+            Intent intent = new Intent(this, SignupActivity.class);
+            startActivity(intent);
+        }
+    }
